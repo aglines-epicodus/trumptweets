8011b854d4f8d9ba31440468ec1d31b910957ab0
Andrew Glines <aglines@gmail.com>
2017-09-08 19:29:14 -0700

    Add initial files

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Tweet a compliment sandwich to @realDonaldTrump</h2>
+        </div>
+        <p className="App-intro">
+
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+
+        <div>Nail Down: </div>
+        <div>Firebase</div>
+        <div>Bootstrap</div>
+
+      </div>
+    );
+  }
+}
+
+export default App;
